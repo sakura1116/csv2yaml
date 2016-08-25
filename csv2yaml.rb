472303f01b886ab8c4a5ce02c9502d5155aede4d
sakura1116 <daisuke172-60@hotmail.co.jp>
2016-08-25 17:39:41 +0900

    first commit

---
+++ csv2yaml.rb
@@ -0,0 +1,19 @@
+require 'csv'
+require 'yaml'
+require 'pp'
+
+#path_to_csv = 'city_code.csv'
+path_to_csv = 'city_code_special_city.csv'
+#path_to_yaml = 'city_code.yaml'
+path_to_yaml = 'city_code_special_city.yaml'
+
+csv = CSV.read(path_to_csv, :headers => true).map(&:to_hash)
+
+hash = {}
+csv.each do |row|
+   city = row['city_code']
+   hash[city] = row
+end
+
+File.open(path_to_yaml, 'w') { |f| f.write(hash.to_yaml) }
+pp yaml = YAML.load_file(path_to_yaml)
